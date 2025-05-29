9700e0bf6891b14b0bfe7f02f1f809f6b50c0fd9
Velichko Slavilov <velichkoslavilov98@gmail.com>
2025-05-29 23:31:06 +0300

    Add files via upload

---
+++ Sirma Academy Java - Loops/src/Clock.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Clock {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 23; i++){
+            for(int j = 0; j <= 59; j++){
+                System.out.println(i+":"+j);
+            }
+        }
+
+    }
+}
