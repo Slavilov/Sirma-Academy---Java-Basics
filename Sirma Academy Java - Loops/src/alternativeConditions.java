cc79ea3ba1115eb5abc214a4c3351c9587eddbb7
Velichko Slavilov <velichkoslavilov98@gmail.com>
2025-06-03 23:53:10 +0300

    Add files via upload

---
+++ Sirma Academy Java - Loops/src/alternativeConditions.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class alternativeConditions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 1;
+
+        for (int i = 1; num <= n; i++) {
+            for (int j = 0; j < n - i; j++) System.out.print("  ");
+            for (int j = 0; j < i && num <= n; j++) System.out.print(num++ + " ");
+            System.out.println();
+        }
+    }
+}
