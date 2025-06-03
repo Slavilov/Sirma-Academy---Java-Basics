e10c458457d9b80c47a5f70bae7d5539b2e89816
Velichko Slavilov <velichkoslavilov98@gmail.com>
2025-06-03 23:52:45 +0300

    Add files via upload

---
+++ Sirma Academy Java - Loops/src/pyramidOfNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class pyramidOfNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 1;
+
+        for (int i = 1; num <= n; i++) {
+            for (int j = 0; j < i && num <= n; j++) {
+                System.out.print(num++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
