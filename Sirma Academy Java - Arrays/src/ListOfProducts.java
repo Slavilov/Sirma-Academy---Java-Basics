10fa6cd093c878ffd52a24303a64c5141e6eb9da
Velichko Slavilov <velichkoslavilov98@gmail.com>
2025-06-08 23:16:47 +0300

    Add files via upload

---
+++ Sirma Academy Java - Arrays/src/ListOfProducts.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ListOfProducts {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] products = scanner.nextLine().split(",");
+        Arrays.sort(products);
+
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i + 1) + "." + products[i].trim());
+        }
+    }
+}
