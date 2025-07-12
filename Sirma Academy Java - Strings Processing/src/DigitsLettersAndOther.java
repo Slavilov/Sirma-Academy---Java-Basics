7dc2e4a974609c81b94c8f1c1f2fe227739c2418
Velichko Slavilov <velichkoslavilov98@gmail.com>
2025-07-12 18:47:04 +0300

    Add files via upload

---
+++ Sirma Academy Java - Strings Processing/src/DigitsLettersAndOther.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercise05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        StringBuilder digits = new StringBuilder();
+        StringBuilder letters = new StringBuilder();
+        StringBuilder others = new StringBuilder();
+
+        for (char c : text.toCharArray()) {
+            if (Character.isDigit(c)) digits.append(c);
+            else if (Character.isLetter(c)) letters.append(c);
+            else others.append(c);
+        }
+
+        System.out.println(digits);
+        System.out.println(letters);
+        System.out.println(others);
+    }
+}
